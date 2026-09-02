35119420edcaca181d049a12e8e536b640ef466c
Gorkem Tosun <gorkem.tosun@thougthworks.com>
2026-09-02 16:19:13 +0300

    Add Dialog component built on Radix

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,3 +3,14 @@ export { Button, type ButtonProps } from "./components/Button";
 export { Badge, type BadgeProps } from "./components/Badge";
 export { Input, type InputProps } from "./components/Input";
 export { Notice, type NoticeProps } from "./components/Notice";
+export {
+  Dialog,
+  DialogTrigger,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  type DialogContentProps,
+} from "./components/Dialog";
